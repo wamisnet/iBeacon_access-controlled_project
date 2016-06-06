ae187d5ba4a427e8a4363e7b26f8fab343b6f812
ogi1517 <ogi1517@gmail.com>
2016-06-06 12:21:14 +0900

    Service+

--- android/out/MyApplication/app/src/main/java/com/example/ogi/myapplication/MyService.java
+++ android/out/MyApplication/app/src/main/java/com/example/ogi/myapplication/MyService.java
@@ -11,6 +11,8 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.nifty.cloud.mb.core.NCMB;
+
 public class MyService  extends Service {
     BluetoothAdapter mBluetoothAdapter;
     Handler mHandler = new Handler();
@@ -24,6 +26,8 @@ public class MyService  extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("TAG", "onCreate");
+        NCMB.initialize(this.getApplicationContext(),"8eee2292f5c87bae5ec5bbb3bdb95ee997708bd1bc96aed8f8ed6f142ce71e61",
+                "29aea4c9781e3664e4f9c959c2e04074ab3f765c74586ed447947699a5385970");
     }
 
 
