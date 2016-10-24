c5d12a504ac9c1b0eca838741e6dde1b386b9e55
ogi1517 <ogi1517@gmail.com>
2016-10-24 09:50:38 +0900

    next timer [OK]

--- android/out/MyApplication/app/src/main/java/com/example/ogi/myapplication/SampleServices.java
+++ android/out/MyApplication/app/src/main/java/com/example/ogi/myapplication/SampleServices.java
@@ -16,8 +16,8 @@ public class SampleServices extends IntentService {
 
     int StartHour[] = {9,10,13,14,0};
     int StartTime[] = {15,55,15,55,00};
-    private static int dStartHour[] = {10,11,12,13,0};//デバッグ用
-    private static int dStartTime[] = {3,4,5,8,00};
+    private static int dStartHour[] = {9,9,12,13,0};//デバッグ用
+    private static int dStartTime[] = {48,50,5,8,00};
 
     private static final String TAG = "SampleService";
 
@@ -28,6 +28,8 @@ public class SampleServices extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         Log.d("時間テスト", "time:" + SystemClock.elapsedRealtime());
+        //スキャン後タイマー設定
+        startAlarm(getApplicationContext());
     }
 
     /**
@@ -47,24 +49,26 @@ public class SampleServices extends IntentService {
                 new Intent(context, SampleServices.class),
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
-        // 10秒毎にサービスの処理を実行する
         AlarmManager am = (AlarmManager) context
                 .getSystemService(Context.ALARM_SERVICE);
-        /*am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime(), 10 * 1000, pendingIntent);*/
 
         Calendar now = Calendar.getInstance(); //インスタンス化
         now.setTimeInMillis(System.currentTimeMillis());
+        int y = now.get(now.YEAR);       //年を取得
+        int M = now.get(now.MONTH);      //月を取得
+        int d = now.get(now.DATE);       //日を取得
         int h = now.get(now.HOUR_OF_DAY);//時を取得
         int m = now.get(now.MINUTE);     //分を取得
         int s = now.get(now.SECOND);      //秒を取得
-      //  Context w = null;
-                for(x=0 ; x<5 ; x++)//purpose
+                for(x=0 ; x<5 ; x++)//現在時刻と比較し時刻を決める
                 {
                     if(h <= dStartHour[x]) {
                         adtimerhour = dStartHour[x];
                         adtimertime = dStartTime[x];
-                        if(m > dStartTime[x]) {
+                        Log.d("ループ1-dstarthour", String.valueOf(dStartHour[x]));
+                        Log.d("ループ1-dstarttime", String.valueOf(dStartTime[x]));
+
+                        if(m > dStartTime[x] & h == dStartHour[x] ) {
                             x++;
                             adtimerhour = dStartHour[x];
                             adtimertime = dStartTime[x];
@@ -96,7 +100,7 @@ public class SampleServices extends IntentService {
                 }
 
     //Month指定は0から始まる　(ex:1月→0 2月→1
-        now.set(2016,9,21,adtimerhour,adtimertime,s);
+        now.set(y,M,d,adtimerhour,adtimertime,s);
         Log.d("設定前-adtimerhour", String.valueOf(adtimerhour));
         Log.d("設定前-adtimertime", String.valueOf(adtimertime));
         am.set(AlarmManager.RTC_WAKEUP, now.getTimeInMillis(), pendingIntent);
