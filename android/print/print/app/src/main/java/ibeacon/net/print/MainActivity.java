3d9428f4580358b3271facdf37b8918c96e85b64
ogi1517 <ogi1517@gmail.com>
2016-11-17 13:25:44 +0900

    1117

--- android/print/print/app/src/main/java/ibeacon/net/print/MainActivity.java
+++ android/print/print/app/src/main/java/ibeacon/net/print/MainActivity.java
@@ -9,7 +9,6 @@ import com.nifty.cloud.mb.core.NCMB;
 import com.nifty.cloud.mb.core.NCMBException;
 import com.nifty.cloud.mb.core.NCMBObject;
 import com.nifty.cloud.mb.core.NCMBQuery;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,21 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NCMB.initialize(this.getApplicationContext(),"8eee2292f5c87bae5ec5bbb3bdb95ee997708bd1bc96aed8f8ed6f142ce71e61",
-                "29aea4c9781e3664e4f9c959c2e04074ab3f765c74586ed447947699a5385970");
+        NCMB.initialize(this.getApplicationContext(),"fe8cc228956e2f26276c141ce824efb4810c9d711119dcd511e2cd8b39438913",
+                "481f20a51e4ad7d6536280acb04fa83b05023e67105110b36040a221b16f1682");
 
         //TestClassを検索するためのNCMBQueryインスタンスを作成
         NCMBQuery<NCMBObject> query = new NCMBQuery<>("AttendClass");
 
 //keyというフィールドがvalueとなっているデータを検索する条件を設定
-        query.whereEqualTo("major", "65000");
+        query.whereEqualTo("major", 10);
 
 //データストアからデータを検索
         query.findInBackground(new FindCallback<NCMBObject>() {
             @Override
             public void done(List<NCMBObject> results, NCMBException e) {
                 if (e != null) {
-                    Log.d("deta", "err end");
+                    Log.d("deta", "err end");Log.d("deta", String.valueOf(e));
                     //検索失敗時の処理
                 } else {
                   Log.d("deta", String.valueOf(results.get(0)));
